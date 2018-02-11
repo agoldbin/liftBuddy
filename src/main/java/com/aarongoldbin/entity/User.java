4f79173538945948e1d558b7b704d94ecea2e104
Aaron Goldbin <agoldbin@madisoncollege.edu>
2018-02-11 01:14:00 -0600

    initial dao set up for User table

---
+++ src/main/java/com/aarongoldbin/entity/User.java
@@ -0,0 +1,40 @@
+package com.aarongoldbin.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.ejb.Local;
+import javax.persistence.*;
+import java.time.*;
+
+/**
+ * A class to represent a user
+ *
+ * @author agoldbin
+ */
+@Entity(name = "User")
+@Table(name = "user") // case senstitive
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "email")
+    private String userEmail;
+
+//   TODO Check how to pull gym name from gym table or if it is unneeded in this class
+    @Column(name = "gym_id")
+    private int gym;
+
+//    TODO Decide if location will be zip, city or both
+    @Column(name = "location")
+    private String userLocation;
+
+//    TODO is password needed to be stored? How to store a password hash
+    @Column(name = "password")
+    private String password;
+}
