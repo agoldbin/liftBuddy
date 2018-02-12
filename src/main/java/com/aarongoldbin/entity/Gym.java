714049ef84254f64c36fc1afd02a8328f81723db
Aaron Goldbin <agoldbin@madisoncollege.edu>
2018-02-12 00:16:46 -0600

    fleshing out project dao tests, javabeans, and dao classes

--- src/main/java/com/aarongoldbin/entity/Gym.java
+++ src/main/java/com/aarongoldbin/entity/Gym.java
@@ -1,4 +1,24 @@
 package com.aarongoldbin.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * A class to represent a gym choices
+ *
+ * @author agoldbin
+ */
+@Entity(name = "liftbuddytest")
+@Table(name = "gym") // case senstitive
+
 public class Gym {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    @Column(name = "gym_id")
+    private int gymId;
+
+    @Column(name = "gym_name")
+    private String gymName;
 }
