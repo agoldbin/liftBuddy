5203190555f2d37084f0681f3c67f61774679acc
Aaron Goldbin <agoldbin@madisoncollege.edu>
2018-02-13 03:04:11 -0600

    homework at work is a nuisance

--- src/main/java/com/aarongoldbin/entity/Gym.java
+++ src/main/java/com/aarongoldbin/entity/Gym.java
@@ -16,8 +16,8 @@ public class Gym {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
-    @Column(name = "gym_id")
-    private int gymId;
+    @OneToMany(mappedBy = "gymId", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private int id;
 
     @Column(name = "gym_name")
     private String gymName;
--- src/main/java/com/aarongoldbin/entity/Profile.java
+++ src/main/java/com/aarongoldbin/entity/Profile.java
@@ -19,25 +19,208 @@ public class Profile {
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "weight")
     private int weight;
-
-    @Column(name = "height")
     private int height;
-
-    @Column(name = "age")
     private int age;
-
-    @Column(name = "sex")
     private String sex;
-
-    @Column(name = "goal")
     private String goal;
 
     @Column(name = "searching_for")
     private int searching;
 
-    @Column(name = "id")
+    @OneToOne
     private int id;
 
+    /**
+     * Instantiates a new Profile.
+     */
+    public Profile() {
+    }
+
+    /**
+     * Instantiates a new Profile.
+     *
+     * @param firstName the first name
+     * @param lastName  the last name
+     * @param weight    the weight
+     * @param height    the height
+     * @param age       the age
+     * @param sex       the sex
+     * @param goal      the goal
+     * @param searching the searching
+     * @param id        the id
+     */
+    public Profile(String firstName, String lastName, int weight, int height, int age, String sex, String goal, int searching, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.weight = weight;
+        this.height = height;
+        this.age = age;
+        this.sex = sex;
+        this.goal = goal;
+        this.searching = searching;
+        this.id = id;
+    }
+
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets weight.
+     *
+     * @return the weight
+     */
+    public int getWeight() {
+        return weight;
+    }
+
+    /**
+     * Sets weight.
+     *
+     * @param weight the weight
+     */
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    /**
+     * Gets height.
+     *
+     * @return the height
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Sets height.
+     *
+     * @param height the height
+     */
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    /**
+     * Gets age.
+     *
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Sets age.
+     *
+     * @param age the age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * Gets sex.
+     *
+     * @return the sex
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * Sets sex.
+     *
+     * @param sex the sex
+     */
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * Gets goal.
+     *
+     * @return the goal
+     */
+    public String getGoal() {
+        return goal;
+    }
+
+    /**
+     * Sets goal.
+     *
+     * @param goal the goal
+     */
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    /**
+     * Gets searching.
+     *
+     * @return the searching
+     */
+    public int getSearching() {
+        return searching;
+    }
+
+    /**
+     * Sets searching.
+     *
+     * @param searching the searching
+     */
+    public void setSearching(int searching) {
+        this.searching = searching;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
 }
