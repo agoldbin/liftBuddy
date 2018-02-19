8483339495b1a324b525ef57406855d3074e7882
Aaron Goldbin <31373437+agoldbin@users.noreply.github.com>
2018-02-18 19:55:26 -0600

    Added a few more tests to both DaoTest classes

--- src/test/java/com/aarongoldbin/persistence/UserDaoTest.java
+++ src/test/java/com/aarongoldbin/persistence/UserDaoTest.java
@@ -11,8 +11,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// TODO update this file. Was just copypastad from week1, test set up is incorrect
-
 class UserDaoTest {
 
     UserDao dao;
@@ -26,7 +24,6 @@ class UserDaoTest {
     void setUp() {
         dao = new UserDao();
 
-        System.out.println("@BeforeEach");
         Database database = Database.getInstance();
         database.runSQL("cleandb.sql");
     }
@@ -39,10 +36,13 @@ class UserDaoTest {
         User retrievedUser = dao.getById(3);
         assertEquals("bcurry", retrievedUser.getUserName());
         assertEquals("curryman@yahoo.com", retrievedUser.getUserEmail());
-        assertEquals("53704", retrievedUser.getUserLocation());
+        assertEquals("53704", retrievedUser.getLocation());
         assertEquals(3, retrievedUser.getId());
     }
 
+    /**
+     * Verify that new user is successfully added
+     */
     @Test
     void insertNewUserSuccess() {
         GymDao gymDao = new GymDao();
@@ -54,7 +54,7 @@ class UserDaoTest {
         User insertedUser = dao.getById(id);
         assertEquals("TMoney", insertedUser.getUserName());
         assertEquals("teddymo@gmail.com", insertedUser.getUserEmail());
-        assertEquals("53219", insertedUser.getUserLocation());
+        assertEquals("53219", insertedUser.getLocation());
 
         // TODO review .equals recommendations http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
     }
@@ -95,10 +95,10 @@ class UserDaoTest {
      */
     @Test
     void getByPropertyEqualSuccess() {
-  /*      List<User> users = dao.getByPropertyLike("lastName", "Curry");
+        List<User> users = dao.getByPropertyLike("lastName", "Curry");
         assertEquals(1, users.size());
         assertEquals(3, users.get(0).getId());
-    */
+
     }
 
     /**
@@ -108,5 +108,24 @@ class UserDaoTest {
     void getByPropertyLikeSuccess() {
         List<User> users = dao.getByPropertyLike("userName", "c");
         assertEquals(3, users.size());
+    }
+
+    /**
+     * Verify successful finding of users in a location
+     */
+    @Test
+    void getUsersFromLocationSuccess() {
+        List<User> users = dao.getByPropertyLike("location", "71");
+        assertEquals(3, users.size());
+        assertEquals(6, users.get(2).getId());
+    }
+
+    /**
+     * Verify that all users are successfully found
+     */
+    @Test
+    void getAllUsersSuccess() {
+        List<User> users = dao.getAllUsers();
+        assertEquals(6, users.size());
     }
 }
